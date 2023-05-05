ed5658120c3d0d78a1d47331e7122e18b943c2bc
omonsoanasi <emmanuelomonso@gmail.com>
2023-05-05 13:52:43 +0300

    fix the candidate homepage redirect

--- app/Http/Controllers/Candidate/CandidateIndexController.php
+++ app/Http/Controllers/Candidate/CandidateIndexController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Candidate;
 
 use App\Http\Controllers\Controller;
+use App\Models\StaffRequistionForm;
 use Illuminate\Http\Request;
 
 class CandidateIndexController extends Controller
 {
+//    public function index()
+//    {
+//        return view('candidate.index');
+//    }
     public function index()
     {
-        return view('candidate.index');
+        $availablejobs = StaffRequistionForm::where('status',4)->where('status', true)->whereDate('applicationdeadline', '>=', now())->get();
+        return view('candidate.availablejobs.index', compact('availablejobs'));
     }
 }
