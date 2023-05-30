faf159f4b686f2077980a705a507f71848d0ed21
omonsoanasi <emmanuelomonso@gmail.com>
2023-05-30 10:49:00 +0300

    feat: changed the authentication to allow users to use LDAP to sign in

--- resources/views/auth/app.blade.php
+++ resources/views/auth/app.blade.php
@@ -2,7 +2,7 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
+    <form method="POST" action="{{ route('login') }}" class="bg-cyan-50">
         @csrf
 
         <!-- Email Address -->
@@ -40,7 +40,7 @@
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="https://su-sso.strathmore.edu/staff-pss">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
